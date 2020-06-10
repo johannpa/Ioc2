7c33e5771456345d43b45084c70fa7e0aa0d8bfb
johannpa <johann_paque@hotmail.fr>
2020-06-10 10:22:57 +0200

    c'est mieux que le couplage fort

---
+++ ioc2/src/metier/MetierImpl.java
@@ -0,0 +1,16 @@
+package metier;
+import dao.IDao;
+
+public class MetierImpl implements IMetier{
+	private IDao dao;
+	
+	
+	public double calcul() {
+		double nb=dao.getValue();
+		return 2*nb;
+	}
+	public void setDao(IDao d) {
+		this.dao = d;
+	}
+}
+
